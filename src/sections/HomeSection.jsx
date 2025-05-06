2ed8e6a126861d853a3caf538fc395041050bb4d
melina-escalante <meliescalantee@gmail.com>
2025-05-06 15:26:16 -0300

    fix: link to download resume.

--- src/sections/HomeSection.jsx
+++ src/sections/HomeSection.jsx
@@ -43,11 +43,11 @@ const HomeSection = () => {
 
             </div>  <div className='flex flex-col gap-5 items-center my-8'>
                 <img src="/melina_escalante.jpg" alt="Melina Escalante desarrolladora y diseñadora web."    className='rounded-full h-72 ' />
-                <a title='Dowload Resume' download='Melina-Micaela-Escalante-Resume.pdf' href='/Melina-Micaela-Escalante-CV.pdf' className='p-4 font-semibold flex items-center gap-2 justify-center  rounded-md shadow-button hover:translate-y-[-3px] duration-[0.5s] hover:bg-[#024a7154]'><span><svg class="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                <a title='Dowload Resume' download href='/Melina-Micaela-Escalante-Resume.pdf' className='p-4 font-semibold flex items-center gap-2 justify-center  rounded-md shadow-button hover:translate-y-[-3px] duration-[0.5s] hover:bg-[#024a7154]'><span><svg class="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                     <path fill-rule="evenodd" d="M13 11.15V4a1 1 0 1 0-2 0v7.15L8.78 8.374a1 1 0 1 0-1.56 1.25l4 5a1 1 0 0 0 1.56 0l4-5a1 1 0 1 0-1.56-1.25L13 11.15Z" clip-rule="evenodd" />
                     <path fill-rule="evenodd" d="M9.657 15.874 7.358 13H5a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2h-2.358l-2.3 2.874a3 3 0 0 1-4.685 0ZM17 16a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H17Z" clip-rule="evenodd" />
                 </svg>
-                </span>Download CV</a>
+                </span>Download Resume</a>
             </div>
 
 
