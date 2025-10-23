1d9502964e3109a077facf6d4d5b179c67bcafbc
melina-escalante <meliescalantee@gmail.com>
2025-10-23 12:19:52 -0300

    style: modificar nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,46 +5,16 @@ const NavBar = () => {
     const sections = ['Projects', 'Skills', 'About me']
 
     return (
-        <nav className="relative text-end p-4 z-0">
-            {/* Botón de menú */}
-            <button
-                className=" cursor-pointer  focus:outline-none md:hidden z-500"
-                onClick={() => setIsOpen(!isOpen)}
-            >
-                {/* Icono hamburguesa */}
-                <svg
-                    className="w-6 h-6"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16m-7 6h7"}
-                    />
-                </svg>
-                <span className="sr-only">Menu</span>
-            </button>
-            <div >
-                {/* <div>
-
-                    <a className={`p-4 top-1 absolute md:block left-2.5 ${isOpen?"hidden":"block"} md:left-0 z-50  hover:text-sky-400 hover:font-medium`} href="/">Melina Escalante</a>
-                </div> */}
-
-                <ul
-                    className={`absolute z-0 text-end items-center justify-end md:justify-center top-full left-0 w-full bg-[#121212] h-[100vh] md:h-auto font-medium transform transition-transform duration-500 ease-in-out md:static md:flex md:translate-y-0 md:space-x-4 md:bg-transparent ${isOpen ? "translate-y-[0] -z-100" : "-translate-y-[150%] -z-100"
-                        }`}
+        <nav className="fixed w-full flex justify-center items-center max-w-fit top-8 md:top-5  bg-[#ffffff03]    inset-x-0 mx-auto py-1  px-6 border rounded-full">
+            <ul
+                    className={` flex justify-center  `}
                 >
                     {sections.map((section) => (
                         <li className="p-4 text-white md:hover:bg-transparent " key={section}><a href={`#${section}`} className="hover:text-sky-300 hover:transition-colors focus:ring-2  focus:ring-sky-300 focus:outline-none focus:rounded-sm   ">{section}</a></li>
                     ))}
 
-
                 </ul>
-            </div>
+     
         </nav>
     );
 };
--- src/sections/HomeSection.jsx
+++ src/sections/HomeSection.jsx
@@ -12,7 +12,7 @@ const HomeSection = () => {
                     </svg>`, 'url': 'https://github.com/melinaescalante','name':'Github'}, { 'image': `<svg class="w-6 h-6 fill-white" viewBox="0 0 50 50" ><path d="M12 23.403V23.39 10.389L11.88 10.3h-.01L9.14 8.28C7.47 7.04 5.09 7.1 3.61 8.56 2.62 9.54 2 10.9 2 12.41v3.602L12 23.403zM38 23.39v.013l10-7.391V12.41c0-1.49-.6-2.85-1.58-3.83-1.46-1.457-3.765-1.628-5.424-.403L38.12 10.3 38 10.389V23.39zM14 24.868l10.406 7.692c.353.261.836.261 1.189 0L36 24.868V11.867L25 20l-11-8.133V24.868zM38 25.889V41c0 .552.448 1 1 1h6.5c1.381 0 2.5-1.119 2.5-2.5V18.497L38 25.889zM12 25.889L2 18.497V39.5C2 40.881 3.119 42 4.5 42H11c.552 0 1-.448 1-1V25.889z"/></svg>`, 'name': 'Gmail' }]
     return (<>
         <NavBar></NavBar>
-        <section id="Home" className="grid grid-cols-1 md:grid-cols-2 p-4 md:p-0 items-center max-w-screen-lg my-10 md:my-20 mt-0 mx-auto">
+        <section id="Home" className="grid grid-cols-1 md:grid-cols-2 p-4 md:p-0 items-center max-w-screen-lg mb-10 pt-34 md:mb-20 md:pt-30 mt-0 mx-auto">
             <div className='flex justify-between flex-col m-2 mb-10 md:mb-0 h-full '>
                 <div className='flex flex-col items-start gap-4  md:p-0 '>
                     <h1 className=' font-bold text-5xl'>Melina Escalante</h1>
