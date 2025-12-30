a17d31953c9e2077a9410d8d06bd2f81ebc808a9
melina-escalante <meliescalantee@gmail.com>
2025-12-29 22:37:39 -0300

    feat: dark mode, styles home

--- src/sections/HomeSection.jsx
+++ src/sections/HomeSection.jsx
@@ -1,12 +1,12 @@
 import NavBar from "../components/NavBar";
 import ThemeContext from "../context/ThemeContext";
-import { use } from "react";
+import { useContext } from "react";
 const HomeSection = () => {
-  const { theme, setTheme } = use(ThemeContext);
+  const { theme, setTheme } = useContext(ThemeContext);
   const socialMedia = [
     {
       image: `<svg class="w-6 h-6 dark:fill-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                        <path fill-rule="evenodd" d="M12.51 8.796v1.697a3.738 3.738 0 0 1 3.288-1.684c3.455 0 4.202 2.16 4.202 4.97V19.5h-3.2v-5.072c0-1.21-.244-2.766-2.128-2.766-1.827 0-2.139 1.317-2.139 2.676V19.5h-3.19V8.796h3.168ZM7.2 6.106a1.61 1.61 0 0 1-.988 1.483 1.595 1.595 0 0 1-1.743-.348A1.607 1.607 0 0 1 5.6 4.5a1.601 1.601 0 0 1 1.6 1.606Z" clip-rule="evenodd" />
+                        <path fillRule="evenodd" d="M12.51 8.796v1.697a3.738 3.738 0 0 1 3.288-1.684c3.455 0 4.202 2.16 4.202 4.97V19.5h-3.2v-5.072c0-1.21-.244-2.766-2.128-2.766-1.827 0-2.139 1.317-2.139 2.676V19.5h-3.19V8.796h3.168ZM7.2 6.106a1.61 1.61 0 0 1-.988 1.483 1.595 1.595 0 0 1-1.743-.348A1.607 1.607 0 0 1 5.6 4.5a1.601 1.601 0 0 1 1.6 1.606Z" clipRule="evenodd" />
                         <path d="M7.2 8.809H4V19.5h3.2V8.809Z" />
                     </svg>`,
       url: "https://www.linkedin.com/in/melina-escalante-93b024291/",
@@ -14,7 +14,7 @@ const HomeSection = () => {
     },
     {
       image: `<svg class="w-6 h-6  dark:fill-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                        <path fill-rule="evenodd" d="M12.006 2a9.847 9.847 0 0 0-6.484 2.44 10.32 10.32 0 0 0-3.393 6.17 10.48 10.48 0 0 0 1.317 6.955 10.045 10.045 0 0 0 5.4 4.418c.504.095.683-.223.683-.494 0-.245-.01-1.052-.014-1.908-2.78.62-3.366-1.21-3.366-1.21a2.711 2.711 0 0 0-1.11-1.5c-.907-.637.07-.621.07-.621.317.044.62.163.885.346.266.183.487.426.647.71.135.253.318.476.538.655a2.079 2.079 0 0 0 2.37.196c.045-.52.27-1.006.635-1.37-2.219-.259-4.554-1.138-4.554-5.07a4.022 4.022 0 0 1 1.031-2.75 3.77 3.77 0 0 1 .096-2.713s.839-.275 2.749 1.05a9.26 9.26 0 0 1 5.004 0c1.906-1.325 2.74-1.05 2.74-1.05.37.858.406 1.828.101 2.713a4.017 4.017 0 0 1 1.029 2.75c0 3.939-2.339 4.805-4.564 5.058a2.471 2.471 0 0 1 .679 1.897c0 1.372-.012 2.477-.012 2.814 0 .272.18.592.687.492a10.05 10.05 0 0 0 5.388-4.421 10.473 10.473 0 0 0 1.313-6.948 10.32 10.32 0 0 0-3.39-6.165A9.847 9.847 0 0 0 12.007 2Z" clip-rule="evenodd" />
+                        <path fillRule="evenodd" d="M12.006 2a9.847 9.847 0 0 0-6.484 2.44 10.32 10.32 0 0 0-3.393 6.17 10.48 10.48 0 0 0 1.317 6.955 10.045 10.045 0 0 0 5.4 4.418c.504.095.683-.223.683-.494 0-.245-.01-1.052-.014-1.908-2.78.62-3.366-1.21-3.366-1.21a2.711 2.711 0 0 0-1.11-1.5c-.907-.637.07-.621.07-.621.317.044.62.163.885.346.266.183.487.426.647.71.135.253.318.476.538.655a2.079 2.079 0 0 0 2.37.196c.045-.52.27-1.006.635-1.37-2.219-.259-4.554-1.138-4.554-5.07a4.022 4.022 0 0 1 1.031-2.75 3.77 3.77 0 0 1 .096-2.713s.839-.275 2.749 1.05a9.26 9.26 0 0 1 5.004 0c1.906-1.325 2.74-1.05 2.74-1.05.37.858.406 1.828.101 2.713a4.017 4.017 0 0 1 1.029 2.75c0 3.939-2.339 4.805-4.564 5.058a2.471 2.471 0 0 1 .679 1.897c0 1.372-.012 2.477-.012 2.814 0 .272.18.592.687.492a10.05 10.05 0 0 0 5.388-4.421 10.473 10.473 0 0 0 1.313-6.948 10.32 10.32 0 0 0-3.39-6.165A9.847 9.847 0 0 0 12.007 2Z" clipRule="evenodd" />
                     </svg>`,
       url: "https://github.com/melinaescalante",
       name: "Github",
@@ -29,7 +29,7 @@ const HomeSection = () => {
       <NavBar></NavBar>
       <button
         onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-        className="cursor-pointer hover:bg-blue-100 hover:border-blue-100 hover:transition-colors duration-[0.5s] border-1 border-blue-900 rounded-full p-2 left-2 fixed top-5"
+        className="cursor-pointer hover:bg-blue-100 hover:border-blue-100 hover:transition-colors duration-500 border border-blue-900 rounded-full p-2 left-2 fixed top-5"
       >
         {theme === "light" ? (
           <svg
@@ -42,9 +42,9 @@ const HomeSection = () => {
             viewBox="0 0 24 24"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M11.675 2.015a.998.998 0 0 0-.403.011C6.09 2.4 2 6.722 2 12c0 5.523 4.477 10 10 10 4.356 0 8.058-2.784 9.43-6.667a1 1 0 0 0-1.02-1.33c-.08.006-.105.005-.127.005h-.001l-.028-.002A5.227 5.227 0 0 0 20 14a8 8 0 0 1-8-8c0-.952.121-1.752.404-2.558a.996.996 0 0 0 .096-.428V3a1 1 0 0 0-.825-.985Z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             />
           </svg>
         ) : (
@@ -58,16 +58,16 @@ const HomeSection = () => {
             viewBox="0 0 24 24"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M13 3a1 1 0 1 0-2 0v2a1 1 0 1 0 2 0V3ZM6.343 4.929A1 1 0 0 0 4.93 6.343l1.414 1.414a1 1 0 0 0 1.414-1.414L6.343 4.929Zm12.728 1.414a1 1 0 0 0-1.414-1.414l-1.414 1.414a1 1 0 0 0 1.414 1.414l1.414-1.414ZM12 7a5 5 0 1 0 0 10 5 5 0 0 0 0-10Zm-9 4a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2H3Zm16 0a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2h-2ZM7.757 17.657a1 1 0 1 0-1.414-1.414l-1.414 1.414a1 1 0 1 0 1.414 1.414l1.414-1.414Zm9.9-1.414a1 1 0 0 0-1.414 1.414l1.414 1.414a1 1 0 0 0 1.414-1.414l-1.414-1.414ZM13 19a1 1 0 1 0-2 0v2a1 1 0 1 0 2 0v-2Z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             />
           </svg>
         )}
       </button>
       <section
-        id="Home"
-        className="grid grid-cols-1 md:grid-cols-2 p-4 md:p-0 items-center max-w-screen-lg mb-10 pt-34 md:mb-20 md:pt-45 mt-0 mx-auto"
+        id="home"
+        className="grid grid-cols-1 md:grid-cols-2 p-4 md:p-0 items-center max-w-5xl mb-10 pt-34 md:mb-20 md:pt-45 mt-0 mx-auto"
       >
         <div className="flex justify-around flex-col m-2 mb-10 md:mb-0 h-full ">
           <div className="flex flex-col items-start gap-6  md:p-0 ">
@@ -78,12 +78,12 @@ const HomeSection = () => {
               </span>
             </h1>
             <div className="flex flex-col gap-6">
-              <p className="break-words text-l">
+              <p className="wrap-break-word">
 
                 Passionate about creating functional, intuitive and attractive
                 websites and applications.
               </p>
-              <p className="break-words text-l">
+              <p className="wrap-break-word">
                 I create designs personalized to each client, prioritizing their
                 preferences and accessibility.
               </p>
@@ -109,7 +109,7 @@ const HomeSection = () => {
                     ? "mailto:meliescalantee@gmail.com"
                     : social.url
                 }
-                className=" dark:border-transparent border border-blue-800 p-4 rounded-md shadow-icon hover:translate-y-[-3px] duration-[0.5s] hover:bg-blue-400/15 hover:border-transparent"
+                className=" dark:border-transparent border border-blue-500 p-4 rounded-md shadow-icon hover:translate-y-[-3px] duration-500 hover:bg-blue-400/15 hover:border-transparent"
               >
                 <span dangerouslySetInnerHTML={{ __html: social.image }} />
                 <span className="sr-only">{social.name}</span>
@@ -129,7 +129,7 @@ const HomeSection = () => {
             title="Dowload Resume"
             download
             href="/Resume-Melina-Micaela-Escalante.pdf"
-            className="p-4 font-semibold flex items-center gap-2 justify-center  rounded-md shadow-button hover:translate-y-[-3px] duration-[0.5s] hover:bg-blue-400/15"
+            className="p-4 font-semibold flex items-center gap-2 justify-center  rounded-md shadow-button hover:translate-y-[-3px] duration-500 hover:bg-blue-400/15"
             >
             <span>
               <svg
@@ -142,14 +142,14 @@ const HomeSection = () => {
                 viewBox="0 0 24 24"
                 >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M13 11.15V4a1 1 0 1 0-2 0v7.15L8.78 8.374a1 1 0 1 0-1.56 1.25l4 5a1 1 0 0 0 1.56 0l4-5a1 1 0 1 0-1.56-1.25L13 11.15Z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                   />
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M9.657 15.874 7.358 13H5a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2h-2.358l-2.3 2.874a3 3 0 0 1-4.685 0ZM17 16a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H17Z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                   />
               </svg>
             </span>
