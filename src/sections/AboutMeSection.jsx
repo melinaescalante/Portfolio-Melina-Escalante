568af4f66d47917f1dc85990cd62e05281431c06
melina-escalante <meliescalantee@gmail.com>
2025-12-29 22:34:09 -0300

    style: text about me

--- src/sections/AboutMeSection.jsx
+++ src/sections/AboutMeSection.jsx
@@ -1,6 +1,6 @@
 const AboutMeSection = () => {
     return (<>
-        <section id="About%20me" className="max-w-screen-lg mx-auto">
+        <section id="about-me" className="max-w-screen-lg mx-auto my-28">
 
 
             <h2 className="text-4xl mb-8 text-start md:text-center ms-4 font-poppins  font-medium">About me</h2>
@@ -10,9 +10,9 @@ const AboutMeSection = () => {
                 </div>  
                 <div className="flex flex-col gap-5 leading-8 p-4 col-span-2">
 
-                    <p>My name is <span className='font-bold  text-blue-400'>Melina Escalante</span>, and my journey in web development began in 2020, during high school, where I graduated with a <span className='font-bold  text-blue-400'>Bachelor's degree in Computer Science</span>. In 2025, I graduated as a <span className='font-bold  text-blue-400'>Full Stack Developer</span> and continue to expand my knowledge in technologies like React.js.</p>
+                    <p>My name is <span className='font-bold  text-blue-400'>Melina Escalante</span>, and my journey in web development began in 2020, during high school, where I graduated with a <span className='font-bold  text-blue-400'>Bachelor's degree in Computer Science</span>. In 2025, I graduated as a <span className='font-bold  text-blue-400'>Full Stack Developer</span> and currently pursuing a Bachelor’s degree in Systems.</p>
                     <p>I am an ambitious and optimistic person, passionate about challenges and constant learning. My goal is to offer <span className='font-bold  text-blue-400'>efficient and customized solutions</span> to each client.</p>
-                    <p>I'm currently seeking my first job opportunity in web development. If you are interested in working with me, <span className='font-bold  text-blue-400'>please don't hesitate to contact me!</span></p>
+                    <p>If you are interested in working with me, <span className='font-bold  text-blue-400'>please don't hesitate to contact me!</span></p>
                 </div>
 
 
