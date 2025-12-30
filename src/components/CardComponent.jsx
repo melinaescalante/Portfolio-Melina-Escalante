636d729fee2ae2cd6626a3fedae239bb46b39a7d
melina-escalante <meliescalantee@gmail.com>
2025-12-29 22:39:08 -0300

    style: dom card

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,16 +1,24 @@
 const CardComponent = ({ obj }) => {
   return (
-    <div className="border border-blue-400 hover: flex flex-col rounded-lg justify-between shadow-card hover:translate-y-[-5px] hover:bg-[#024a7154] hover: duration-[0.5s] hover:bg-icon ">
+    // hover:bg-[#024a7154]
+    <div className={`border border-blue-400 hover: flex flex-col rounded-lg justify-between shadow-card hover:translate-y-[-5px]  hover:duration-500 hover:bg-icon ${obj.styles}`}>
       <div className="flex flex-col ">
-        <img
-          className="h-40 border-2 border-amber-200 rounded-tl-lg rounded-tr-lg  "
-          src="https://endlessicons.com/wp-content/uploads/2012/11/image-holder-icon-614x460.png"
-          alt=""
-        />
-        <h3 className="flex text-lg font-medium break-words px-2">
+        <div className="w-full overflow-hidden rounded-t-lg">
+          <span className="absolute z-10 bg-black/60 text-white text-xs px-2 py-1 m-2 rounded">
+           {obj.type}
+          </span>
+
+          <img
+            src={obj.image? obj.image:"https://endlessicons.com/wp-content/uploads/2012/11/image-holder-icon-614x460.png"}
+            alt=""
+            className="w-full h-40 object-cover "
+          />
+        </div>
+
+        <h3 className="flex text-lg font-medium break-words px-1">
           <a
             href={obj.url}
-            className=" focus:outline-none focus:rounded-sm focus:ring-blue-300 flex items-center gap-2 p-2 hover:underline hover:decoration-1"
+            className=" focus:outline-none focus:rounded-sm focus:ring-blue-300 flex items-center gap-2 p-2 py-1 hover:underline hover:decoration-1"
             target="_blank"
           >
             <span>{obj.title}</span>
@@ -24,36 +32,28 @@ const CardComponent = ({ obj }) => {
                 viewBox="0 0 24 24"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M12.006 2a9.847 9.847 0 0 0-6.484 2.44 10.32 10.32 0 0 0-3.393 6.17 10.48 10.48 0 0 0 1.317 6.955 10.045 10.045 0 0 0 5.4 4.418c.504.095.683-.223.683-.494 0-.245-.01-1.052-.014-1.908-2.78.62-3.366-1.21-3.366-1.21a2.711 2.711 0 0 0-1.11-1.5c-.907-.637.07-.621.07-.621.317.044.62.163.885.346.266.183.487.426.647.71.135.253.318.476.538.655a2.079 2.079 0 0 0 2.37.196c.045-.52.27-1.006.635-1.37-2.219-.259-4.554-1.138-4.554-5.07a4.022 4.022 0 0 1 1.031-2.75 3.77 3.77 0 0 1 .096-2.713s.839-.275 2.749 1.05a9.26 9.26 0 0 1 5.004 0c1.906-1.325 2.74-1.05 2.74-1.05.37.858.406 1.828.101 2.713a4.017 4.017 0 0 1 1.029 2.75c0 3.939-2.339 4.805-4.564 5.058a2.471 2.471 0 0 1 .679 1.897c0 1.372-.012 2.477-.012 2.814 0 .272.18.592.687.492a10.05 10.05 0 0 0 5.388-4.421 10.473 10.473 0 0 0 1.313-6.948 10.32 10.32 0 0 0-3.39-6.165A9.847 9.847 0 0 0 12.007 2Z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 />
               </svg>
             </span>
           </a>
         </h3>
       </div>
-      <p className="mx-3 font-normal">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto iste
-        quam nostrum, aut, sequi dolorum non impedit magnam reprehenderit
-        doloribus quasi dolor cumque. Cum, placeat laborum? Recusandae eaque
-        velit omnis?
+      <p className="mx-3 font-normal ">
+        {obj.description}
       </p>
-      {/* <p
-        className="
-        pt-0 p-2 my-0"
-        dangerouslySetInnerHTML={{ __html: obj.description }}
-      ></p> */}
       {obj.title === "E-commerce desing" && (
         <a
           href="https://www.figma.com/proto/TZZeSDoGz8YI69vKW4WYN5/Prototipado-Luana-Beauty?node-id=23-2&p=f&t=bRN1vF5kO45fMe4C-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1"
           target="_blank"
-          className="hover:underline hover:decoration-1 font-bold ps-2 focus:ring-2 focus:outline-none focus:rounded-sm focus:ring-blue-200 "
+          className="hover:underline hover:decoration-1 font-bold ps-3 mt-1.5 "
         >
           Prototype
         </a>
       )}
-      <div className="flex flex-wrap p-2">
+      <div className="flex flex-wrap p-2 py-1">
         {obj.tecnologies.map((tec) => (
           <div
             key={tec.name}
